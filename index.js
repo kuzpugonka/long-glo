e00e4a43e96b1d52f2ea83a64aae25c4b0a3710f
kuzpugonka <6599255@bk.ru>
2026-07-09 19:19:14 +0300

    second home work

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "строка с названием проекта";
+let screens =
+  "строка с названиями типов экранов через запятую ('Простые, Сложные, Интерактивные')";
+let screenPrice = 5;
+let rollback = 15;
+let fullPrice = 70000;
+let adaptive = true;
+const cashback = fullPrice * (rollback / 100);
 
-alert("Любой текст");
-console.log("Любой текст!");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+console.log(
+  "Стоимость верстки экранов " + screenPrice + " рублей/долларов/гривен/юани",
+);
+console.log(
+  "Стоимость разработки сайта " + fullPrice + " рублей/долларов/гривен/юани",
+);
+console.log(screens.toLowerCase());
+console.log(screens.split(" "));
+console.log(
+  "Процент отката посреднику за работу " +
+    cashback +
+    " рублей/долларов/гривен/юани",
+);
