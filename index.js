7af9c3a0ea54201d7edbbbbed300851cba96e605
kuzpugonka <6599255@bk.ru>
2026-07-11 14:06:53 +0300

    исправила ошибку в getRollbackMessage

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ const showTypeOf = (variable) => {
 const getRollbackMessage = (price) => {
   if (price >= 30000) {
     return "Даем скидку в 10%";
-  } else if (price >= 15000 && fullPrice < 30000) {
+  } else if (price >= 15000 && price < 30000) {
     return "Даем скидку в 5%";
-  } else if (price >= 0 && fullPrice < 15000) {
+  } else if (price >= 0 && price < 15000) {
     return "Скидка не предусмотрена";
   } else {
     return "Что-то пошло не так";
