a82d69bd8fb16c3f58b2678230c03a1a112d3f67
kuzpugonka <6599255@bk.ru>
2026-07-11 14:04:31 +0300

    повтор кода из урока

--- index.js
+++ index.js
@@ -14,16 +14,27 @@ const rollback = 15;
 const fullPrice = screenPrice + servicePrice1 + servicePrice2;
 const servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback / 100));
 
-if (fullPrice >= 30000) {
-  console.log("Даем скидку в 10%");
-} else if (fullPrice >= 15000 && fullPrice < 30000) {
-  console.log("Даем скидку в 5%");
-} else if (fullPrice >= 0 && fullPrice < 15000) {
-  console.log("Скидка не предусмотрена");
-} else {
-  console.log("Что-то пошло не так");
-}
+const showTypeOf = (variable) => {
+  console.log(variable, typeof variable);
+};
 
+const getRollbackMessage = (price) => {
+  if (price >= 30000) {
+    return "Даем скидку в 10%";
+  } else if (price >= 15000 && fullPrice < 30000) {
+    return "Даем скидку в 5%";
+  } else if (price >= 0 && fullPrice < 15000) {
+    return "Скидка не предусмотрена";
+  } else {
+    return "Что-то пошло не так";
+  }
+};
+
+showTypeOf (title);
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
+
+console.log(getRollbackMessage(fullPrice));
 console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
@@ -31,4 +42,10 @@ console.log(typeof adaptive);
 console.log(screens.length);
 console.log("servicePercentPrice: ", servicePercentPrice);
 
-console.log('Стоимость верски экранов ' + screenPrice + " руб.");
+console.log(
+  "Стоимость верски экранов " +
+    screenPrice +
+    " руб. Стоимость разработки сайта " +
+    servicePercentPrice +
+    " руб.",
+);
