145cb9d6fec933852a475c2372a636a5350a2a45
kuzpugonka <6599255@bk.ru>
2026-07-11 13:31:06 +0300

    привела в соответствие с образцом

--- index.js
+++ index.js
@@ -12,28 +12,23 @@ const service2 = prompt("Какой дополнительный тип услу
 const servicePrice2 = +prompt("Сколько это будет стоить?");
 const rollback = 15;
 const fullPrice = screenPrice + servicePrice1 + servicePrice2;
-const servicePercentPrice = fullPrice - fullPrice * (rollback / 100);
+const servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback / 100));
 
 if (fullPrice >= 30000) {
   console.log("Даем скидку в 10%");
 } else if (fullPrice >= 15000 && fullPrice < 30000) {
   console.log("Даем скидку в 5%");
-} else if (fullPrice >= 0 && fullPrice < 15000 ) {
+} else if (fullPrice >= 0 && fullPrice < 15000) {
   console.log("Скидка не предусмотрена");
 } else {
   console.log("Что-то пошло не так");
 }
 
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
 
-if (title != null) console.log("title: ", title);
-if (screens != null) console.log("screens: ", screens);
-if (screenPrice != null) console.log("screenPrice: ", screenPrice);
-if (adaptive != null) console.log("adaptive: ", adaptive);
-if (service1 != null) console.log("service1: ", service1);
-if (servicePrice1 != null)
-  console.log("servicePrice1: ", servicePrice1 + " руб.");
-if (service2 != null) console.log("service2: ", service2);
-if (servicePrice2 != null)
-  console.log("servicePrice2: ", servicePrice2 + " руб.");
-console.log("fullPrice: ", fullPrice + " руб.");
-console.log("servicePercentPrice: ", Math.ceil(servicePercentPrice) + " руб.");
+console.log(screens.length);
+console.log("servicePercentPrice: ", servicePercentPrice);
+
+console.log('Стоимость верски экранов ' + screenPrice + " руб.");
